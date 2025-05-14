cfcbe952ec0b13b4b0dacdcf3918479d3a10c43a
Alejandro Alsina <89251424+Ale-Alsina-UNLP@users.noreply.github.com>
2025-05-14 18:38:52 -0300

    Resolución mostrar los dígitos (“0” al “9”)

---
+++ Practicas/TP01/CODIGOS/TP01_P2_Ej03b.asm
@@ -0,0 +1,12 @@
+     org 1000h
+num  db "0"
+     org 2000h
+       mov bx, offset num
+       mov al, 1
+print: int 7
+       cmp num, 39h ; "9"
+       jz fin
+       inc num
+       jmp print
+fin:   int 0
+end
