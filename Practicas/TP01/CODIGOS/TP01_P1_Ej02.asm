00c14e1eab87358b3cd7c9504f16a3e248522093
Alejandro Alsina <89251424+Ale-Alsina-UNLP@users.noreply.github.com>
2025-05-14 08:13:48 -0300

    Create TP01_P1_Ej02.asm

---
+++ Practicas/TP01/CODIGOS/TP01_P1_Ej02.asm
@@ -0,0 +1,14 @@
+        org 1000h
+C   db 'A'
+RES db ?
+       org 2000h
+       mov al,C
+       cmp al,41h
+       js noMay
+       cmp al,5Ah
+       js noMay
+       mov RES, 00h
+       jmp fin
+noMay: mov RES,0FFh
+fin:   int 0
+end
