8a54da08ce4d7c3336ecfc97e5a2a08c37698b01
Alejandro Alsina <89251424+Ale-Alsina-UNLP@users.noreply.github.com>
2025-05-16 05:28:04 -0300

    Resolución Convertir String a Minúsculas

--- Practicas/TP01/CODIGOS/TP01_P1_Ej04.asm
+++ Practicas/TP01/CODIGOS/TP01_P1_Ej04.asm
@@ -1,11 +1,11 @@
-    org 1000h
-msj  db "Hola, Buenas Tardes"
-fin  db ?
-    org 2000h
+  ORG 1000h
+msj db "HoLA, BuEnAs TaRDeS"
+fin db ?
+  ORG 2000h
        mov bx, offset msj
        mov cl, offset fin - offset msj
-       mov al, 41h ; "a"
-       mov ah, 5ah ; "z"
+       mov al, 41h
+       mov ah, 5ah
 sigue: mov ch, [bx]
        cmp ch, al
        js no_es
@@ -15,5 +15,8 @@ sigue: mov ch, [bx]
 no_es: inc bx
        dec cl
        jnz sigue
+mov bx, offset msj
+mov al, offset fin - offset msj 
+int 7
 hlt
-end
+END
