0bd141f2886930eaf833a3bef10e14b4a1072365
Alejandro Alsina <89251424+Ale-Alsina-UNLP@users.noreply.github.com>
2025-05-15 21:55:38 -0300

    Resolución subrutina A_MINUS

---
+++ Practicas/TP01/CODIGOS/TP01_P4_Ej03c.asm
@@ -0,0 +1,16 @@
+  ORG 1000H
+c   db "g"
+res db ?
+
+;;;;;; SIBRUTINA A_MINUS ;;;;;;
+  ORG 3000H
+minus: add al, 20h
+       ret
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+ ORG 2000H
+mov al, c
+call minus
+mov res, al
+hlt
+end
