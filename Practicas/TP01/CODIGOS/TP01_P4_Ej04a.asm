d2e5f4d3b2b96aa6b278dbba4c3dca0d71dfac67
Alejandro Alsina <89251424+Ale-Alsina-UNLP@users.noreply.github.com>
2025-05-15 22:36:38 -0300

    Resolución Multiplicación num s/signo por param por valor resul por valor via REG

---
+++ Practicas/TP01/CODIGOS/TP01_P4_Ej04a.asm
@@ -0,0 +1,35 @@
+  org 1000h
+a   db 100
+b   db 3
+res dw ?
+
+;;;;;;;;;;;;;;;; SUBRUTINA Multiplicación ;;;;;;;;;;;;;;;;;;;
+; parámetros por valor a través de los registros AL y AH
+; devolviendo el resultado a través del registro AX por valor
+  org 3000h
+mul:   push cx
+       push dx
+       mov dx, 0
+       cmp al, 0
+       jz salir
+       cmp ah, 0
+       jz salir
+       mov ch, 0
+       mov cl, al
+
+loop:  add dx, cx
+       dec ah
+       jnz loop
+salir: mov ax, dx
+       pop dx
+       pop cx
+       ret
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  org 2000h
+mov al, a
+mov ah, b
+call mul
+mov res, ax
+hlt
+end
