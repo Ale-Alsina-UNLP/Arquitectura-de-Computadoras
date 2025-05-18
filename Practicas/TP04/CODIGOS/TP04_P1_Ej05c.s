ac14d3902ba6b8ee2ac7f46428a7c5db42e4e3a6
Alejandro Alsina <89251424+Ale-Alsina-UNLP@users.noreply.github.com>
2025-05-18 20:41:23 -0300

    Resolución s/nemo, cálculo factorial de N

---
+++ Practicas/TP04/CODIGOS/TP04_P1_Ej05c.s
@@ -0,0 +1,23 @@
+;<------------- TP04_P1_Ej05 B ------------>
+; SIN NEMOTECNICOS SOLO NRO REGISTROS
+; Calcula el factorial de N, 
+; y lo guarda en F:
+;  F = 1
+;  FOR i= 1..N:
+;    F = F * i
+;-------------------------------------------
+
+    .data
+N:  .word 5
+F:  .word 0
+
+    .code
+     ld    R8,  N(R0)
+     daddi R9,  R0, 1
+     daddi R10, R0, 1
+FAC: dmul  R10, R10, R9
+     daddi R9,  R9, 1
+     daddi R8,  R8, -1
+     bnez  R8, FAC
+     sd R10, F(R0)
+halt
